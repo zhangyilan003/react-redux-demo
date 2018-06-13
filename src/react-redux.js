f4a5d0131d4b0fb9e7dec34612e6279d7947772e
zhangyilan <1952945642@qq.com>
2018-06-13 18:02:14 +0800

    up connect 和 mapStateToProps ，高阶组件的使用

--- src/react-redux.js
+++ src/react-redux.js
@@ -3,17 +3,36 @@ import PropTypes from 'prop-types'
 
 export const connect = (mapStateToProps) => (WrappedComponent) => {
     class Connect extends Component {
-        static contextTypes = {
-            store:PropTypes.object
-        }
-        //todo:如何从store取数据？
-
-        render () {
-            const { store } = this.context
-            let stateProps = mapStateToProps(store.getState())
-            // {...stateProps} 意思是把这个对象里面的属性全部通过 `props` 方式传递进去
-            return <WrappedComponent {...stateProps} />
-        }
+      static contextTypes = {
+        store: PropTypes.object
+      }
+  
+      constructor () {
+        super()
+        this.state = { allProps: {} }
+      }
+  
+      componentWillMount () {
+        const { store } = this.context
+        this._updateProps()
+        store.subscribe(() => this._updateProps())
+      }
+  
+      _updateProps () {
+        const { store } = this.context
+        let stateProps = mapStateToProps(store.getState(), this.props) // 额外传入 props，让获取数据更加灵活方便
+        this.setState({
+          allProps: { // 整合普通的 props 和从 state 生成的 props
+            ...stateProps,
+            ...this.props
+          }
+        })
+      }
+  
+      render () {
+        return <WrappedComponent {...this.state.allProps} />
+      }
     }
+  
     return Connect
-}
+  }
